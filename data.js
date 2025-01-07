fe071baa1ae2d2ad812b882e8b840de8abb374db
akshara-sp <95979133+akshara-sp@users.noreply.github.com>
2025-01-06 23:56:39 -0800

    first draft

---
+++ data.js
@@ -0,0 +1,153 @@
+// export const bio = [
+//   "Hi, I’m Akshara Pillai, a Software Developer with a passion for building efficient and user-friendly applications. With expertise in languages  and frameworks.",
+//    "I create innovative solutions that deliver impact.",
+
+// "Explore my work and let’s connect to build something extraordinary!",
+
+//   "Thanks for visiting!",
+// ];
+
+export const bio = [
+  "🎓 Pursuing an MS in Computer Science at the University of Colorado Boulder, focusing on data science applications and strengthening my expertise in computing fundamentals.",
+  "💼 3+ years of professional experience as a software developer, with expertise in building web applications, APIs, and data analysis tools. Skilled in frontend and backend development, with additional experience in SAP ERP solutions.",
+  "💻 Proficient in technologies including Python, JavaScript, FastAPI, ReactJS, SQL, Docker, and more. Demonstrated ability to design, develop, and deploy scalable software solutions.",
+  "🚀 Actively seeking 2025 opportunities for internships, co-ops, or full-time roles in dynamic and innovative environments where I can contribute my skills and continue to grow."
+]
+
+export const skills = [
+    {
+      title: "Programming Languages",
+      skillName: "Python, Java, C++, JavaScript, SQL, Shell Scripting",
+      color: "1",
+      percentage: "99",
+    },
+    {
+      title: "Front-End Development",
+      skillName: "React.js, TypeScript, Redux, HTML, CSS, XML, Bootstrap, Tailwind",
+      color: "6",
+      percentage: "95",
+    },
+    {
+      title: "Back-End Development",
+      skillName: "Node.js, MongoDB, Flask, FastAPI",
+      color: "2",
+      percentage: "90",
+    },
+    {
+      title: "DevOps & Tools",
+      skillName: "Docker, Kubernetes, Jenkins, Git, AWS, Firebase, Linux",
+      color: "3",
+      percentage: "90",
+    },
+    {
+      title: "Testing & Automation",
+      skillName: "Selenium, Webpack, Jira",
+      color: "7",
+      percentage: "85",
+    },  
+];
+
+export const projects = {
+  webProjects: [
+    {
+      projectName: "BOOK-FINDER-AI",
+      summary:
+        "Designed and developed a personalized book recommendation website powered by ChatGPT. The website features a curated selection of two or three books for users to choose from and based on their preferences for these books, ChatGPT generates a list of recommended books.",
+      techStack: ["ReactJS", "Redux", "Firebase"],
+    },
+    {
+      projectName: "BUDGET TRACKER",
+      summary:
+        "Designed and developed a budget tracker website that provides users with a comprehensive expense tracking system, allowing them to monitor their spending habits. Users can set monthly budgets, ensuring better financial management. The website also offers interactive visualizations, enabling users to gain insights from their past expenses.",
+      techStack: ["ASP.NET", "C#", "SQL"],
+    },
+  ],
+  softwareProjects: [
+    {
+      projectName: "Food Scanning using Image Processing",
+      summary:
+        "Image processing methods used to detect and discard spoiled fruits and vegetables. Arduino is used for controlling the hardware which consisted of a conveyor belt, sensors and camera. MATLAB was used for image recognition and classification.",
+      techStack: ["Matlab"],
+    },
+  ],
+};
+
+export const experience = [
+  {
+    title: "Our Future Reads",
+    duration: "March 2023 - April 2024",
+    subtitle: "Software Developer",
+    details: [
+      'Volunteer as a Software Developer at the non-profit organization "Our Future Reads" to support the website for The Access Program (TAP) platform.',
+      "Designed and implemented frontend components & FastAPI based REST APIs to enhance the functionality and user experience of TAP's platform.",
+      'Conducted thorough testing and debugging to identify and resolve issues, ensuring a robust API infrastructure.'
+    ],
+    tags: ["FastAPI", "SolidJS", "Tailwind CSS", "SQL", "XML", "Python", "Webpack", "Docker"],
+    icon: "group",
+  },
+  {
+    title: "KRIBHCO",
+    duration: "Sept 2019 - Oct 2021 ",
+    subtitle: "Software Engineer",
+    details: [
+      'Acquired in-depth knowledge of KRIBHCO',
+      'Business operations processed and used SAP software applications to design business process simulation with the help of SAP consultants.',
+      'Developed and maintained modules for internal dashboard and data analysis tools',
+      'Conducted detailed quality assessments, identifying and resolving reliability issues.',
+      'Gained comprehensive understanding of SAP business operations across various verticals addressing the specific needs of the company.',
+    ],
+  tags: [
+    "ReactJS",
+    "Python",
+    "SAP",
+    "ERP",
+    "SQL",
+    "Selenium",
+  ],
+  icon: "group",
+  },
+
+];
+
+export const education = [
+  {
+    title: "Master of Science in Computer Science",
+    duration: "September 2024 - Present",
+    subtitle: "University of Colorado Boulder, Boulder",
+    details: [],
+    tags: ["Python, Advanced Data Structures, Algorithms"],
+    icon: "book",
+  },
+  {
+    title: "Full Stack Web & Mobile Development Bootcamp",
+    duration: "October 2021 - March 2022",
+    subtitle: "Nucamp",
+    details: [],
+    tags: ["Javascript, HTML, CSS, XML, Bootstrap, React.js, Node, Express, MongoDB, Docker"],
+    icon: "graduation-cap",
+
+  },
+  {
+    title: "Bachelor of Technology in Electronics and Communications",
+    duration: "August 2015 - June 2019 ",
+    subtitle: "Babaria Institute of Technology (BITS), Vadodara, India",
+    details: [],
+    tags: ["C, C++, Matlab"],
+    icon: "graduation-cap",
+  },
+];
+
+export const footer = [
+  
+  {
+    label: "Links",
+    data: [
+      ,
+      
+      {
+        text: "www.linkedin.com/in/akshara-sivan-pillai",
+        link: "https://www.linkedin.com/in/akshara-sivan-pillai",
+      },
+    ],
+  },
+];
